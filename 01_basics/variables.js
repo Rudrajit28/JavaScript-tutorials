856dea8c5f0a4bc3c6c0e6e4288b9275c3974321
Rudrajit28 <153164299+Rudrajit28@users.noreply.github.com>
2026-08-18 21:26:47 +0000

    variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,12 @@
+const account_id=234556
+var account_email="rudrajit@email.com"
+let account_city="punjab"
+let account_name
+
+//account_id=34
+
+account_city="Tripura"
+account_email="rudra@gmail.com"
+console.log(account_city)
+console.table([account_id,account_email,account_city,account_name])
+// to make variables we use only let and const( forget about var becoz of scope issues in block and functions )
